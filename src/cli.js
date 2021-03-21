024206ac639462ae46c0e516ad98628030c462d8
Nils pettersson <nils-pettsson@outlook.com>
2021-03-21 10:55:43 +0100

    added some code to parse commands

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,31 @@
+import arg from "arg";
+
+function parseArguments(rawArgs) {
+    let args = arg(
+        {
+            '--help': Boolean,
+            '--version': Boolean,
+            '-h': '--help',
+            '-v': '--version'
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
+    return {
+        help: args['--help'] || false,
+        version: args['--version'] || false,
+    }
+}
+
+function getCommand(index, args){
+    return args[index + 2];
+}
 
 export function cli(args) {
-    console.log(args);
+    if(getCommand(0, args) == "init") {
+        console.log("starting stuff");
+    }
+    
+    console.log(parseArguments(args));
 }
