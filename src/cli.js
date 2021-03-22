ce6f22f56b82406f2d888bcf30dccae804bc20c2
Nils pettersson <nils-pettsson@outlook.com>
2021-03-22 22:47:39 +0100

    added command layout

--- src/cli.js
+++ src/cli.js
@@ -1,31 +1,24 @@
-import arg from "arg";
+let yargs = require('yargs');
 
-function parseArguments(rawArgs) {
-    let args = arg(
-        {
-            '--help': Boolean,
-            '--version': Boolean,
-            '-h': '--help',
-            '-v': '--version'
+export function cli(rawArgs) {
+    let args = yargs
+    .option("help", {
+        alias: "h",
+    })
+    .option("version", {
+        alias: "v",
+        global: false
+    })
+    .command("init", "Initilize a new project", {
+        type: {
+            alias: "t",
+            describe: "The type of project",
+            choices: "other"
         },
-        {
-            argv: rawArgs.slice(2),
-        }
-    );
-    return {
-        help: args['--help'] || false,
-        version: args['--version'] || false,
-    }
-}
+    });
 
-function getCommand(index, args){
-    return args[index + 2];
-}
-
-export function cli(args) {
-    if(getCommand(0, args) == "init") {
-        console.log("starting stuff");
+    if(rawArgs.length == 2 || args.argv.help) {
+        args.showHelp();
     }
     
-    console.log(parseArguments(args));
 }
