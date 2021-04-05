cffbb34220746c2242978d360c8334742f54ddd7
Nils pettersson <nils-pettsson@outlook.com>
2021-04-05 17:22:18 +0200

    added public class that handles client img js and css

--- src/commands/public.js
+++ src/commands/public.js
@@ -3,11 +3,32 @@ import { Command } from "./base/command";
 let shell = require('shelljs');
 
 export class Public extends Command {
-    constructor() {
-        super("public");
+    constructor(args) {
+        super("public", "Manages js, css and images", {}, args);
+    }
+
+    execute(args) {
+        new Js(args).getCommand();
+        new JsWatch(args).getCommand();
+    }
+}
+
+class Js extends Command {
+    constructor(args) {
+        super("js", "Build js", {}, args);
     }
 
     execute() {
-        //shell.exec("npm run --prefix project/public js-w");
+        shell.exec("npm run --prefix project/public js");
+    }
+}
+
+class JsWatch extends Command {
+    constructor(args) {
+        super("js-w", "Build and watch js", {}, args);
+    }
+
+    execute() {
+        shell.exec("npm run --prefix project/public js-w");
     }
 }
